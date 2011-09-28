fa236b7d802caf70c87a96d8d91ce5464416378a
Manuel Kaufmann <humitos@gmail.com>
2011-09-27 21:10:46 -0300

    Some utils added
     - removed auto-completion from ryan, it doesn't work to me
     - highlight the current line
     - line numbers on the left
     - binding to find next and prev errors for flymake
     - highlight backets
     - open automatically rope project

--- emacs-init-file.el
+++ emacs-init-file.el
@@ -218,93 +218,6 @@
 (setq yas/trigger-key (kbd "C-c <kp-multiply>"))
 
 
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;                                         
-;; ;;; Auto-completion                                                                                            
-;; ;;;  Integrates:                                                                                               
-;; ;;;   1) Rope                                                                                                  
-;; ;;;   2) Yasnippet                                                                                             
-;; ;;;   all with AutoComplete.el                                                                                 
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;                                         
-
-(defun prefix-list-elements (list prefix)
-  (let (value)
-    (nreverse
-     (dolist (element list value)
-      (setq value (cons (format "%s%s" prefix element) value))))))
-
-(defvar ac-source-rope
-  '((candidates
-     . (lambda ()
-         (prefix-list-elements (rope-completions) ac-target))))
-  "Source for Rope")
-
-(defun ac-python-find ()
-  "Python `ac-find-function'."
-  (require 'thingatpt)
-  (let ((symbol (car-safe (bounds-of-thing-at-point 'symbol))))
-    (if (null symbol)
-        (if (string= "." (buffer-substring (- (point) 1) (point)))
-            (point)
-          nil)
-      symbol)))
-
-(defun ac-python-candidate ()
-  "Python `ac-candidates-function'"
-  (let (candidates)
-    (dolist (source ac-sources)
-      (if (symbolp source)
-          (setq source (symbol-value source)))
-      (let* ((ac-limit (or (cdr-safe (assq 'limit source)) ac-limit))
-             (requires (cdr-safe (assq 'requires source)))
-             cand)
-        (if (or (null requires)
-                (>= (length ac-target) requires))
-            (setq cand
-                  (delq nil
-                        (mapcar (lambda (candidate)
-                                  (propertize candidate 'source source))
-                                (funcall (cdr (assq 'candidates source)))))))
-        (if (and (> ac-limit 1)
-                 (> (length cand) ac-limit))
-            (setcdr (nthcdr (1- ac-limit) cand) nil))
-        (setq candidates (append candidates cand))))
-    (delete-dups candidates)))
-
-(add-hook 'python-mode-hook
-          (lambda ()
-                 (auto-complete-mode 1)
-                 (set (make-local-variable 'ac-sources)
-                      (append ac-sources '(ac-source-rope)))
-                 (set (make-local-variable 'ac-find-function) 'ac-python-find)
-                 (set (make-local-variable 'ac-candidate-function) 'ac-python-candidate)
-                 (set (make-local-variable 'ac-auto-start) t)))
-
-(global-set-key (kbd "C-x c") 'auto-complete)
-
-
-;; Ryan's python specific tab completion                                                                        
-(defun ryan-python-tab ()
-  ; Try the following:                                                                                         
-  ; 1) Do a yasnippet expansion                                                                                
-  ; 2) Do a Rope code completion                                                                               
-  ; 3) Do an indent                                                                                            
-  (interactive)
-  (if (eql (ac-start) 0)
-      (python-indent-line)))
-
-(defadvice ac-start (before advice-turn-on-auto-start activate)
-  (set (make-local-variable 'ac-auto-start) t))
-(defadvice ac-cleanup (after advice-turn-off-auto-start activate)
-  (set (make-local-variable 'ac-auto-start) nil))
-
-(define-key python-mode-map "\t" 'ryan-python-tab)
-
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;                                         
-;; ;;; End Auto Completion                                                                                        
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; (require 'auto-complete-python)
-
 ;; http://www.emacswiki.org/SmartOperator
 ;; (require 'smart-operator)
 ;; (smart-operator-mode 1)
@@ -356,3 +269,39 @@
 
 ;; Showing Popups
 ;; http://emacs-fu.blogspot.com/2009/11/showing-pop-ups.html
+
+
+;; Highlight current line
+;; http://www.gnu.org/software/emacs/manual/html_node/emacs/Cursor-Display.html#index-highlight-current-line-613
+(global-hl-line-mode 1)
+(set-face-background 'hl-line "seashell2") ;; Nice color
+
+;; Highlight brackets
+;; http://www.emacswiki.org/emacs/ShowParenMode
+(show-paren-mode t)
+
+;; Highlight indentation
+;; http://www.emacswiki.org/emacs/HighlightIndentation
+(require 'highlight-indentation)
+(add-hook 'python-mode-hook 'highlight-indentation)
+
+;; Line numbering
+(setq linum-format "%4d")
+(global-linum-mode 1)
+
+
+;; Key binding to go to Next and Previous error with Flymake
+(global-set-key [f10] 'flymake-goto-prev-error)
+(global-set-key [f11] 'flymake-goto-next-error)
+
+;; Adding hook to automatically open a rope project if there is one
+;; in the current or in the upper level directory
+;; https://github.com/gabrielelanaro/emacs-for-python/blob/master/epy-python.el#L30
+(add-hook 'python-mode-hook
+	  (lambda ()
+	    (cond ((file-exists-p ".ropeproject")
+		   (rope-open-project default-directory))
+		  ((file-exists-p "../.ropeproject")
+		   (rope-open-project (concat default-directory "..")))
+		  )))
+)
