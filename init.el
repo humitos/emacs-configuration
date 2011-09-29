39802c143d482a291ad10f3ed793efe867c73b94
Manuel Kaufmann <humitos@gmail.com>
2011-09-29 09:26:10 -0300

    A new way to lunch emacs configuration
    I've been starting to migrate the config init to initializate all my personal configuration with a single file per module.
    
    Besides, I added git submodules for each of them.

---
+++ init.el
@@ -0,0 +1,16 @@
+;; init.el configuration file
+
+;; Load all extra libraries from externals
+(add-to-list 'load-path "~/.emacs.d/vendor")
+(let ((default-directory "~/.emacs.d/vendor"))
+  (normal-top-level-add-subdirs-to-load-path))
+
+;; Set path for startup.d
+(add-to-list 'load-path "~/.emacs.d")
+
+;; Load all my personal configurations
+(require 'startupd)
+;; Set path for personal configurations
+(setq startupd-path "~/.emacs.d/startup.d/")
+(startupd-load-files)
+
---
+++ startup.d/zenburn-theme.el
@@ -0,0 +1,10 @@
+;; Personal configuration for zenburn-theme.el
+
+;; Set the color theme
+;; https://github.com/bbatsov/zenburn-emacs
+;; http://www.emacswiki.org/emacs/ColorThemeZenburn
+
+;; FIXME: I need to auto-accept the question about "Are you sure that
+;; you want to excecute the theme Lisp code?"
+(add-to-list 'custom-theme-load-path "~/.emacs.d/vendor/zenburn-emacs")
+(load-theme 'zenburn)
