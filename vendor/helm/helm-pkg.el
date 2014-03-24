1f30812473bd81bac5412e7c44ef0c09c949e836
Manuel Kaufmann <humitos@gmail.com>
2014-03-24 13:20:18 -0300

    Revert "Added vendor/helm"
    This reverts commit 1855a54a0fd0f36a2096f9a7d63b06aa5f25a46b.

--- vendor/helm/helm-pkg.el
+++
@@ -1,9 +0,0 @@
-;;; helm-pkg.el --- define helm for package.el -*- lexical-binding: t -*-
-
-(define-package "helm" "1.6.0"
-  "Helm is an Emacs incremental and narrowing framework")
-
-;; Local Variables:
-;; byte-compile-warnings: (not cl-functions obsolete)
-;; no-byte-compile: t
-;; End:
