bb25fca6e981117c4a5339086229dce01e2b131d
Manuel Kaufmann <humitos@gmail.com>
2014-04-22 13:01:06 -0300

    Emacs client

---
+++ startup.d/default.el
@@ -0,0 +1,1 @@
+(setq inhibit-startup-message t)
---
+++ startup.d/emacsclient.el
@@ -0,0 +1,14 @@
+; save the position inside the file
+(require 'saveplace)
+(setq server-visit-hook (quote (save-place-find-file-hook)))
+
+; end an emacsclient session
+(add-hook 'server-switch-hook
+	  (lambda ()
+	    (when (current-local-map)
+	      (use-local-map (copy-keymap (current-local-map))))
+	    (when server-buffer-clients
+	      (local-set-key (kbd "C-x k") 'server-edit))))
+
+; start the server
+(server-start)
