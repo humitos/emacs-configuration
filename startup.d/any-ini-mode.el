0dc40c1e3995d2c0baf195a95d61eb25265790bc
Manuel Kaufmann <humitos@gmail.com>
2016-05-29 18:42:56 -0500

    Proper configuration of any-ini-mode

--- startup.d/any-ini-mode.el
+++ startup.d/any-ini-mode.el
@@ -1,1 +1,5 @@
 (require 'any-ini-mode)
+
+(add-to-list 'auto-mode-alist '(".*\\.ini$" . any-ini-mode))
+(add-to-list 'auto-mode-alist '(".*\\.conf$" . any-ini-mode))
+(add-to-list 'auto-mode-alist '(".*rc$" . any-ini-mode))
