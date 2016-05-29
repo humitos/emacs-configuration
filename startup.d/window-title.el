ff09c3ca532185515b52e6ce77e5c7b7cd99aaa3
Manuel Kaufmann <humitos@gmail.com>
2016-05-29 18:17:25 -0500

    Do not show unnecesary things in window title

--- startup.d/window-title.el
+++ startup.d/window-title.el
@@ -1,6 +1,6 @@
 ;; Changing the Title's Window
 ;; http://emacs-fu.blogspot.com/2011/01/setting-frame-title.html
 (setq frame-title-format
-      '("emacs%@" (:eval (system-name)) ": " (:eval (if (buffer-file-name)
-							(abbreviate-file-name (buffer-file-name))
-						      "%b")) " [%*]"))
+      '((:eval (if (buffer-file-name)
+		   (abbreviate-file-name (buffer-file-name))
+		 "%b"))))
