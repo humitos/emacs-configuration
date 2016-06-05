844e4c82f2d6994cc6afb44f732d2f4969176507
Manuel Kaufmann <humitos@gmail.com>
2016-06-05 18:57:30 -0500

    Some improvements
    * exclude files in projectile (to be linked from other projects)
    * git gutter properly configured
    * added recentf settings to remember recent open files

--- startup.d/git-gutter.el
+++ startup.d/git-gutter.el
@@ -1,9 +1,5 @@
 (require 'git-gutter)
 
-;; show git status (add, remove, etc lines) next to the row numbers
-(global-git-gutter-mode +1)
-
-
 ;; Don't need log/message.
 (custom-set-variables
  '(git-gutter:verbosity 0))
@@ -16,6 +12,7 @@
  '(git-gutter:modified-sign "MM")
  '(git-gutter:added-sign "++")     ;; multiple character is OK
  '(git-gutter:deleted-sign "--")
+
  ;; change name of minor-mode in mode line. first character should be a
  ;; space
  '(git-gutter:lighter " GG")
@@ -26,3 +23,6 @@
 (set-face-background 'git-gutter:added "#263238")
 (set-face-background 'git-gutter:deleted "#263238")
 (set-face-background 'git-gutter:modified "#263238")
+
+;; show git status (add, remove, etc lines) next to the row numbers
+(global-git-gutter-mode +1)
---
+++ startup.d/recentf.el
@@ -0,0 +1,10 @@
+;; https://www.emacswiki.org/emacs/RecentFiles
+(require 'recentf)
+
+;; allow 100 items in the recent file list
+(setq recentf-max-menu-items 100)
+
+;; save the list each 5 minutes (default it's when emacs is killed)
+(run-at-time nil (* 5 60) 'recentf-save-list)
+
+(recentf-mode 1)
