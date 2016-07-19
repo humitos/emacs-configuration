03dbf404dc058d6dae3afc524bac819793c6b4e4
Manuel Kaufmann <humitos@gmail.com>
2016-07-18 20:47:07 -0500

    New flag for custom variable py-docformatter-options

--- startup.d/custom-variables.el
+++ startup.d/custom-variables.el
@@ -15,6 +15,7 @@
      (py-docformatter-options "--wrap-summaries=80" "--wrap-descriptions=80" "--pre-summary-newline" "--no-blank")
      (py-docformatter-options "--wrap-summaries=120 --wrap-descriptions=120 --pre-summary-newline --no-blank")
      (py-docformatter-options "--wrap-summaries=120" "--wrap-descriptions=120" "--pre-summary-newline" "--no-blank")
+     (py-docformatter-options "--wrap-summaries=120" "--wrap-descriptions=120" "--pre-summary-newline" "--no-blank" "--make-summary-multiline")
      (py-autopep8-options "--global-config=/home/humitos/.autopep8rc")
      (py-autopep8-options "--global-config=/home/humitos/mozio/autopep8rc")
      (elpy-test-django-runner-command "/home/humitos/mozio/mozio/manage.py" "test" "--noinput" "--keepdb" "--failfast")
