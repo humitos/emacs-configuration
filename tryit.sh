1fd3dae15a4541dbd0dd1b40a01e4c994f2a1c8e
Manuel Kaufmann <humitos@gmail.com>
2016-09-01 19:48:18 -0500

    Command to try emacs' configuration without installing anything

---
+++ tryit.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+## This shell script it's just a modified version of the one used at:
+## https://github.com/emacs-helm/helm/blob/master/emacs-helm.sh
+
+## Copyright (C) 2012 ~ 2016 Thierry Volpiatto <thierry.volpiatto@gmail.com>
+## 
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+## Commentary:
+
+# Preconfigured Emacs with Manuel Kaufmann (@humitos) configurations
+# Useful to start quickly an emacs -Q with this configuration.
+# Run it from this directory or symlink it somewhere in your PATH.
+
+echo $0
+
+# If TEMP env var exists use it otherwise declare it.
+[ -z $TEMP ] && declare TEMP="/tmp"
+
+CONF_FILE="$TEMP/humitos-cfg.el"
+EMACS=emacs
+
+case $1 in
+    -P)
+        shift 1
+        declare EMACS=$1
+        shift 1
+        ;;
+    -h)
+        echo "Usage: ${0##*/} [-P} Emacs path [-h} help [--] EMACS ARGS"
+        exit 2
+        ;;
+esac
+
+cd $(dirname "$0")
+
+
+cat > $CONF_FILE <<EOF
+(setq initial-scratch-message (concat initial-scratch-message
+";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;\n\
+;; This Emacs is Powered by the humitos configurations\n\
+;; emacs program \"$EMACS\".\n\
+;; This is the full configuration using `https://github.com/humitos/emacs-configuration`.\n\
+;; Please check out the README.rst file to learn about all the packages installed here and the most commons hotkeys.\n\
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;\n\n"))
+
+
+(setq emacs-user-directory (file-name-directory (file-truename "$0")))
+(add-to-list 'load-path (concat (file-name-directory (file-truename "$0")) "packages"))
+(add-to-list 'load-path (concat (file-name-directory (file-truename "$0")) "vendor/startupd.el"))
+
+(require 'startupd)
+;; set path for startup.d configurations
+(setq startupd-path (concat (file-name-directory (file-truename "$0")) "startup.d/"))
+(startupd-load-files)
+
+;; delete temp config file
+(add-hook 'kill-emacs-hook #'(lambda () (and (file-exists-p "$CONF_FILE") (delete-file "$CONF_FILE"))))
+EOF
+
+$EMACS -Q -l $CONF_FILE $@
