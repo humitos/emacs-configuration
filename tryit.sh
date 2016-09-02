6dc7276839300cc5cd0e3b78c7f95d66553d8591
Manuel Kaufmann <humitos@gmail.com>
2016-09-01 22:36:29 -0500

    Improve tryit.sh script for test this settings

--- tryit.sh
+++ tryit.sh
@@ -24,7 +24,8 @@
 # Useful to start quickly an emacs -Q with this configuration.
 # Run it from this directory or symlink it somewhere in your PATH.
 
-echo $0
+# stop the script if something goes wrong
+set -e
 
 # If TEMP env var exists use it otherwise declare it.
 [ -z $TEMP ] && declare TEMP="/tmp"
@@ -52,7 +53,7 @@ cat > $CONF_FILE <<EOF
 ";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;\n\
 ;; This Emacs is Powered by the humitos configurations\n\
 ;; emacs program \"$EMACS\".\n\
-;; This is the full configuration using `https://github.com/humitos/emacs-configuration`.\n\
+;; This is the full configuration using \`https://github.com/humitos/emacs-configuration\`.\n\
 ;; Please check out the README.rst file to learn about all the packages installed here and the most commons hotkeys.\n\
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;\n\n"))
 
@@ -68,6 +69,30 @@ cat > $CONF_FILE <<EOF
 
 ;; delete temp config file
 (add-hook 'kill-emacs-hook #'(lambda () (and (file-exists-p "$CONF_FILE") (delete-file "$CONF_FILE"))))
+
+;; activate emacsenv
+(pyvenv-activate "$0/emacsenv")
 EOF
+
+
+# download default yasnippets
+cd vendor/yasnippet; git submodule init; git submodule update ; cd -
+# disable erc for tryit.sh
+if [ -e startup.d/erc.el ]; then
+  mv --force startup.d/erc.el startup.d/erc.el.disabled > /dev/null
+fi
+# configure helm
+if [ ! -e vendor/helm/helm-autoloads.el ]; then
+  cd vendor/helm ; make ; cd -
+fi
+# create venv
+if [ ! -d emacsenv ]; then
+  python3 -m venv emacsenv
+  source emacsenv/bin/activate
+  pip install -U pip
+  pip install -r requirements.elpy.in
+else
+  source emacsenv/bin/activate
+fi
 
 $EMACS -Q -l $CONF_FILE $@
