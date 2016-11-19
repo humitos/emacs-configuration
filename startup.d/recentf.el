7aff289dac3757ead44841d537bb1dead304e13c
Manuel Kaufmann <humitos@gmail.com>
2016-11-19 12:11:39 -0500

    Improve recentf to save more items and do not cleanup them

--- startup.d/recentf.el
+++ startup.d/recentf.el
@@ -1,10 +1,16 @@
 ;; https://www.emacswiki.org/emacs/RecentFiles
 (require 'recentf)
 
-;; allow 100 items in the recent file list
-(setq recentf-max-menu-items 100)
+;; allow 15 menu items in the recent file list
+(setq recentf-max-menu-items 15)
+(setq recentf-max-saved-items 500)
+
+;; https://github.com/bbatsov/emacs.d/blob/master/init.el
+;; disable recentf-cleanup on Emacs start, because it can cause
+;; problems with remote files
+(setq recentf-auto-cleanup 'never)
 
 ;; save the list each 5 minutes (default it's when emacs is killed)
 ;; (run-at-time nil (* 5 60) 'recentf-save-list)
 
-(recentf-mode 1)
+(recentf-mode +1)
