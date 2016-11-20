34d1398bc749dc19a2db7876456adfe6cba5aafe
Manuel Kaufmann <humitos@gmail.com>
2016-11-19 22:42:08 -0500

    super-save config fixes

--- startup.d/super-save.el
+++ startup.d/super-save.el
@@ -2,5 +2,7 @@
 
 ;; super-save auto-saves your buffers, when certain events happen -
 ;; e.g. you switch between buffers, an Emacs frame loses focus, etc
+(require 'super-save)
 (super-save-mode +1)
-(setq super-save-auto-save-when-idle t)
+
+;; (setq super-save-auto-save-when-idle t)
