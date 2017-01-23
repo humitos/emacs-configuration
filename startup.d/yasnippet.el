6a043c91c21864d76663cc207f7ff5a15e318a6d
Manuel Kaufmann <humitos@gmail.com>
2017-01-23 17:54:33 -0500

    Make TAB works again after yasnippet update

--- startup.d/yasnippet.el
+++ startup.d/yasnippet.el
@@ -1,14 +1,12 @@
 (require 'yasnippet)
-(define-key yas-minor-mode-map [(tab)] nil)
-(define-key yas-minor-mode-map (kbd "TAB") nil)
-(define-key yas-minor-mode-map (kbd "C-i") 'yas-expand)
-;; (define-key yas-minor-mode-map (kbd "C-i") nil)
-
-
 
 (setq yas-snippet-dirs
       (list (concat emacs-user-directory "my-snippets")  ;; personal snippets
 	    (concat emacs-user-directory "vendor/yasnippet/snippets")     ;; default snippet collection
 	    ))
-
 (yas-global-mode 1)
+
+;; (define-key yas-minor-mode-map [(tab)] nil)
+;; (define-key yas-minor-mode-map (kbd "TAB") nil)
+;; (define-key yas-minor-mode-map (kbd "C-i") 'yas-expand)
+;; (define-key yas-minor-mode-map (kbd "C-i") nil)
