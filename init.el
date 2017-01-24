85b41d5f32485aff63a9ace5e9da46ad6a53a271
Manuel Kaufmann <humitos@gmail.com>
2017-01-23 20:41:15 -0500

    Bugfix for emacs-user-directory varialble name

--- init.el
+++ init.el
@@ -1,13 +1,13 @@
 ;; init.el configuration file
 
 ;; Set path for all the libraries/plugins/packages installed
-(add-to-list 'load-path (concat user-emacs-directory "packages"))
+(add-to-list 'load-path (concat emacs-user-directory "packages"))
 
 ;; Set path for startup.d
-(add-to-list 'load-path (concat user-emacs-directory "vendor/startupd.el"))
+(add-to-list 'load-path (concat emacs-user-directory "vendor/startupd.el"))
 
 ;; Load all my personal configurations
 (require 'startupd)
 ;; Set path for personal configurations
-(setq startupd-path (concat user-emacs-directory "startup.d/"))
+(setq startupd-path (concat emacs-user-directory "startup.d/"))
 (startupd-load-files)
