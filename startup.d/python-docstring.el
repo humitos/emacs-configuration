fbf1a968ca794c038f87c003c7cb6473c67e9e2d
Manuel Kaufmann <humitos@gmail.com>
2017-01-26 18:01:21 -0500

    Enable python-docstring-mode for all python-mode buffers

--- startup.d/python-docstring.el
+++ startup.d/python-docstring.el
@@ -5,4 +5,7 @@
 (setq python-docstring-script
       (expand-file-name "vendor/python-docstring-mode/docstring_wrap.py" emacs-user-directory))
 
-(python-docstring-mode t)
+;; enable it in all the python-mode buffers
+(defun python-docstring-mode-hook ()
+  (python-docstring-mode t))
+(add-hook 'python-mode-hook 'python-docstring-mode-hook)
