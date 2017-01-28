02e0abbbb179567c9c607df119c2a2bbcf470d2a
Manuel Kaufmann <humitos@gmail.com>
2017-01-28 02:02:07 -0500

    Update dockerfile name for local build

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -6,7 +6,7 @@ rm -rf repo
 
 python bin/git_archive_all.py --force-submodules repo.tar
 tar xvf repo.tar
-docker build --file Dockerfile-alpine --tag humitos/emacs-x11-alpine .
+docker build --file Dockerfile-local --tag humitos/emacs-x11-alpine .
 
 rm -rf repo.tar
 rm -rf repo
