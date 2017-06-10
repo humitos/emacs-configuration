b94f00fdfc5126d5347890d2b0d31e465261b958
Manuel Kaufmann <humitos@gmail.com>
2017-06-10 20:06:16 -0300

    Do not use double spaces when formatting docstrings in Python

--- startup.d/python-docstring.el
+++ startup.d/python-docstring.el
@@ -5,6 +5,9 @@
 (setq python-docstring-script
       (expand-file-name "vendor/python-docstring-mode/docstring_wrap.py" emacs-user-directory))
 
+;; do not use double spaces when formatting text
+(setq python-docstring-sentence-end-double-space nil)
+
 ;; enable it in all the python-mode buffers
 (defun python-docstring-mode-hook ()
   (python-docstring-mode t))
