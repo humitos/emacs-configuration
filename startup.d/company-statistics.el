4b2e032d254eec8132578197fbdab1918a51b52a
Manuel Kaufmann <humitos@gmail.com>
2017-06-10 20:47:15 -0300

    Enable it manually

--- startup.d/company-statistics.el
+++ startup.d/company-statistics.el
@@ -1,4 +1,4 @@
 ;; https://github.com/company-mode/company-statistics
 (require 'company-statistics)
 (setq company-statistics-size 10000)
-(add-hook 'after-init-hook 'company-statistics-mode)
+(company-statistics-mode)
