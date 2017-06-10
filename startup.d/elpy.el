611aae2c6eed9ad899a7ca2ee811e128a8c3e43f
Manuel Kaufmann <humitos@gmail.com>
2017-06-10 17:27:54 -0300

    Move autopep8 to its own configuration

--- startup.d/elpy.el
+++ startup.d/elpy.el
@@ -17,17 +17,6 @@
 ;; Set timeout for backend rpc
 (setq elpy-rpc-timeout 3)
 
-;; https://github.com/kpurdon/kp-emacs/blob/master/development/_python.el
-;; enable autopep8 formatting on save
-;; ignoring:
-;; - E501 - Try to make lines fit within --max-line-length characters.
-;; - W293 - Remove trailing whitespace on blank line.
-;; - W391 - Remove trailing blank lines.
-;; - W690 - Fix various deprecated code (via lib2to3).
-(require 'py-autopep8)
-(setq py-autopep8-options '("--ignore=E501,W391"))
-(add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
-
 ;; enable newline-and-indent on return
 ;; (define-key global-map (kbd "RET") 'newline-and-indent)
 
--- startup.d/py-autopep8.el
+++ startup.d/py-autopep8.el
@@ -1,2 +1,3 @@
 (require 'py-autopep8)
+(setq py-autopep8-options '((concat "--global-config=" emacs-user-directory "autopep8rc")))
 (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
