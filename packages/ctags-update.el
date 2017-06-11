540d1da22c49ee17d0e4e8854842025771a08993
Manuel Kaufmann <humitos@gmail.com>
2017-06-11 11:32:24 -0700

    Use ctags-update instead of etags-update

---
+++ packages/ctags-update.el
@@ -0,0 +1,1 @@
+../vendor/ctags-update/ctags-update.el
---
+++ startup.d/ctags-update.el
@@ -0,0 +1,8 @@
+;; https://github.com/jixiuf/ctags-update
+(require 'ctags-update)
+(autoload 'turn-on-ctags-auto-update-mode "ctags-update" "turn on `ctags-auto-update-mode'." t)
+(add-hook 'elpy-mode-hook 'turn-on-ctags-auto-update-mode)
+
+(setq ctags-update-command (concat emacs-user-directory  "vendor/ctags/ctags"))
+(setq ctags-update-other-options (list (concat "--options=" emacs-user-directory ".ctags")))
+(setq ctags-update-delay-seconds (* 5 60)) ;; 5 minutes
