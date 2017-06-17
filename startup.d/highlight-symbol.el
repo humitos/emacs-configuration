8c845013306ed802b5c7409fef2aeff99ccaeaa0
Manuel Kaufmann <humitos@gmail.com>
2017-06-17 11:39:07 -0700

    Styling

--- startup.d/highlight-symbol.el
+++ startup.d/highlight-symbol.el
@@ -3,6 +3,7 @@
 (setq highlight-symbol-idle-delay 0.5)
 
 (setq highlight-symbol-ignore-list
-      '("False" "True" "None" "self" "def" "import" "from" "if" "else" "for" "while" "class" "print" "and" "not" "is" "param" "type" "rtype" "async"))
+      '("False" "True" "None" "self" "def" "import" "from" "if" "else" "for"
+        "while" "class" "print" "and" "not" "is" "param" "type" "rtype" "async"))
 
 (add-hook 'elpy-mode-hook 'highlight-symbol-mode)
