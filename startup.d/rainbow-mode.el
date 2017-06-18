9b9f18e38ebaa3472e020a882abb2ba104c90013
Manuel Kaufmann <humitos@gmail.com>
2017-06-18 15:46:47 -0700

    startup.d files organization

--- startup.d/rainbow-mode.el
+++ startup.d/rainbow-mode.el
@@ -1,1 +1,5 @@
+;; This minor mode sets background color to strings that match color
+;; names, e.g. #0000ff is displayed in white with a blue background.
+
+;; https://github.com/emacsmirror/rainbow-mode
 (autoload 'rainbow-mode "rainbow-mode" nil t)
--- startup.d/which-func-mode.el
+++
@@ -1,5 +0,0 @@
-;; (which-function-mode 1)
-
-;; This is not supported in Emacs 25.1
-;; (add-to-list 'which-function-modes 'elpy-mode)
-(setq which-func-unknown "N/A")
