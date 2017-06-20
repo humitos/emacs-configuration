0ad4fc25a30014e8c979423b0d30bda4d75b4fd5
Manuel Kaufmann <humitos@gmail.com>
2017-06-20 15:41:50 -0700

    Split argument to better reading

--- startup.d/savehist.el
+++ startup.d/savehist.el
@@ -3,7 +3,9 @@
 ;; http://emacs-fu.blogspot.com/2009/05/saving-history-between-sessions.html
 ;; http://stackoverflow.com/questions/1229142/how-can-i-save-my-mini-buffer-history-in-emacs
 (setq savehist-additional-variables
-      '(search-ring regexp-search-ring kill-ring))
+      '(search-ring
+        regexp-search-ring
+        kill-ring))
 
 ;; file where to save these things
 (setq savehist-file (concat emacs-user-directory "savehist"))
