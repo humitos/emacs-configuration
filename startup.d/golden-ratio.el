981c0715456cc368966d632c55d390e6ba40da8b
Manuel Kaufmann <humitos@gmail.com>
2017-06-20 20:49:14 -0700

    Do not apply golden-ratio to twittering-edit buffer

--- startup.d/golden-ratio.el
+++ startup.d/golden-ratio.el
@@ -14,6 +14,7 @@
 
 ;; exclude ispell choices when pressing M-$
 (setq golden-ratio-exclude-buffer-names
-      '("*Choices*"))
+      '("*Choices*"
+        "*twittering-edit*"))
 
 (golden-ratio-mode 1)
