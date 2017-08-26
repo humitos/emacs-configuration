55d567b3afc11dc78c38be79c45be1ef99ae102a
Manuel Kaufmann <humitos@gmail.com>
2017-08-26 13:22:34 -0500

    Magit configuration

--- startup.d/magithub.el
+++ startup.d/magithub.el
@@ -1,10 +1,17 @@
 ;; https://github.com/vermiculus/magithub
+(require 'magit)
 (require 'magithub)
 
 (load "~/.ghub.el")
 
 ;; API timeout before asking to go offline
-(setq magithub-api-timeout 5)
+(setq magithub-api-timeout 3)
+
+(setq magithub-cache-class-refresh-seconds-alist
+      '((:issues . 600)
+        (:ci-status . 3600)
+        (:repo-demographics . 86400)
+        (:user-demographics . 86400)
+        (:label . 3600)))
 
 (magithub-feature-autoinject t)
-
