954fb61a4782dd052561cdd27b0993aae0d428e5
Manuel Kaufmann <humitos@gmail.com>
2017-08-27 13:26:18 -0500

    Do not start emacs server if it's already running

--- startup.d/emacsclient.el
+++ startup.d/emacsclient.el
@@ -1,7 +1,9 @@
-; save the position inside the file
+(require 'server)
+
+;; save the position inside the file
 (setq server-visit-hook (quote (save-place-find-file-hook)))
 
-; end an emacsclient session
+;; end an emacsclient session
 (add-hook 'server-switch-hook
 	  (lambda ()
 	    (when (current-local-map)
@@ -9,5 +11,21 @@
 	    (when server-buffer-clients
 	      (local-set-key (kbd "C-x k") 'server-edit))))
 
-; start the server
-(server-start)
+
+;; start the server
+;; `emacs --daemon` in /home/humitos/.config/autostart/emacs-server.desktop
+;;
+;; [Desktop Entry]
+;; Hidden=false
+;; StartupNotify=true
+;; Type=Application
+;; Comment=Emacs Server (daemon)
+;; Terminal=true
+;; GenericName=Emacs Server
+;; X-GNOME-Autostart-enabled=true
+;; Version=1.0
+;; Categories=Utility;
+;; Exec=emacs --daemon
+;; Name=Emacs Server
+(if (not (server-running-p))
+    (server-start))
