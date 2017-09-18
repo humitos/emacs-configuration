b7b3c265b50feb2faa65ed60406d75a119b47d27
Manuel Kaufmann <humitos@gmail.com>
2017-09-18 11:52:00 -0500

    Some twittering-mode tweaks

--- startup.d/twittering-mode.el
+++ startup.d/twittering-mode.el
@@ -5,11 +5,22 @@
 ;; $ gpg --generate-key
 (setq twittering-use-master-password t)
 
+;; unlock the gpg agent at login
+;; https://github.com/cruegge/pam-gnupg
+
+
 ;; my personal style of the timeline
+;; (setq twittering-status-format
+;;       "%i %RT{(RT)} %FACE[bold]{%S},  %@:\n%FOLD[  ]{%T %r%R%QT{\n+----\n%FOLD[|]{%i %S,  %@:\n%FOLD[  ]{%T %r%R}}\n+----}}\n ")
+
 (setq twittering-status-format
-      "%i %RT{(RT)} %FACE[bold]{%S},  %@:\n%FOLD[  ]{%T %r%R%QT{\n+----\n%FOLD[|]{%i %S,  %@:\n%FOLD[  ]{%T %r%R}}\n+----}}\n ")
+      "%i %FACE[bold]{%S} %RT{(RT)}, %@:\n%FOLD[   ]{%T %r%R%QT{\n+--------------------\n%FOLD[|]{%i %S, %@:\n%FOLD[   ]{%T %r%R}}\n+--------------------}}\n ")
 
 ;; remove linum-mode in twittering-mode
 (add-hook 'twittering-mode-hook (lambda () (linum-mode 0)))
 
+;; launch twittering-mode with F10
 (global-set-key (kbd "<f10>") 'twit)
+
+;; inherit mentions and hashtags on editing a reply
+(setq twittering-edit-skeleton 'inherit-any)
