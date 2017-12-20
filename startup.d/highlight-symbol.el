1870353df4055cc3823127dab8b489cf16f7866f
Manuel Kaufmann <humitos@gmail.com>
2017-12-20 11:23:49 -0500

    Type is commonly used, so I want to highlight it

--- startup.d/highlight-symbol.el
+++ startup.d/highlight-symbol.el
@@ -4,7 +4,7 @@
 
 (setq highlight-symbol-ignore-list
       '("False" "True" "None" "self" "def" "import" "from" "if" "else" "for"
-        "while" "class" "print" "and" "not" "is" "param" "type" "rtype" "async"
+        "while" "class" "print" "and" "not" "is" "param" "rtype" "async"
         "with"))
 
 (add-hook 'elpy-mode-hook 'highlight-symbol-mode)
