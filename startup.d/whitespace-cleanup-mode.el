b077a0ae23ec7c03fc6095ab8150497002dddc36
Manuel Kaufmann <humitos@gmail.com>
2018-01-08 11:53:35 -0500

    Better whitespace conf

--- startup.d/whitespace-cleanup-mode.el
+++ startup.d/whitespace-cleanup-mode.el
@@ -1,3 +1,10 @@
 ;; https://github.com/purcell/whitespace-cleanup-mode
 (require 'whitespace-cleanup-mode)
-(whitespace-cleanup-mode 1)
+
+;; this doesn't enable it globally
+;; (whitespace-cleanup-mode 1)
+
+;; enable this plugin by mode
+(add-hook 'python-mode-hook 'whitespace-cleanup-mode)
+(add-hook 'web-mode-hook 'whitespace-cleanup-mode)
+(add-hook 'rst-mode-hook 'whitespace-cleanup-mode)
