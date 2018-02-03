3b44fa23679a7530bc6c5ada719c96dee9a4da31
Manuel Kaufmann <humitos@gmail.com>
2018-02-03 13:41:39 -0500

    Enable nlinum in specific modes only

--- startup.d/00nlinum.el
+++ startup.d/00nlinum.el
@@ -1,5 +1,11 @@
 ;; https://github.com/emacsmirror/nlinum
 (require 'nlinum)
 
-(global-nlinum-mode t) ;; enable line numbers globally
+;; enable line numbers globally
+;; (global-nlinum-mode t)
+
+;; enable nlinum in prog and text mode only
+(add-hook 'prog-mode-hook 'nlinum-mode)
+(add-hook 'text-mode-hook 'nlinum-mode)
+
 (setq nlinum-format "%4d \u2502") ;; line numbers style
