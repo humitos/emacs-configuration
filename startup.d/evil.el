72a7de6b137cfbd855290a8fb7fcbb38537b19e5
Manuel Kaufmann <humitos@gmail.com>
2018-03-17 17:08:30 -0500

    Hotkey for evil mode

--- startup.d/evil.el
+++ startup.d/evil.el
@@ -1,3 +1,7 @@
 ;; https://github.com/emacs-evil/evil
 (require 'evil)
+
+;; use C-g to exit the INSERT mode
+(define-key evil-insert-state-map (kbd "C-g") 'evil-force-normal-state)
+
 (evil-mode 1)
