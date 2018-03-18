078dec3aad998ba61605302a39f056cf877a01d5
Manuel Kaufmann <humitos@gmail.com>
2018-03-17 20:57:14 -0500

    Disable evil mode in Circe

--- startup.d/evil.el
+++ startup.d/evil.el
@@ -4,4 +4,7 @@
 ;; use C-g to exit the INSERT mode
 (define-key evil-insert-state-map (kbd "C-g") 'evil-force-normal-state)
 
+;; Disable evil on Circe
+(evil-set-initial-state 'circe-mode 'emacs)
+
 (evil-mode 1)
