98ad057a78de8560bea009c3164a82cd0af76053
Manuel Kaufmann <humitos@gmail.com>
2018-03-20 20:06:02 -0500

    Remove some minor-modes from mode line

--- startup.d/smart-mode-line.el
+++ startup.d/smart-mode-line.el
@@ -2,4 +2,26 @@
 (require 'smart-mode-line)
 (setq sml/theme 'respectful)
 (setq sml/no-confirm-load-theme t)
+(setq rm-blacklist
+      (format "^ \\(%s\\)$"
+              (mapconcat #'identity
+                         '("Fly.*"
+                           "Projectile.*"
+                           "PgLn"
+                           "Helm"
+                           "hl-s"
+                           "Golden"
+                           "snipe"
+                           "Elpy"
+                           "ctagsU"
+                           "GG"
+                           "Hi"
+                           "Spnxd"
+                           "WSC"
+                           "ws"
+                           "SP"
+                           "DS"
+                           "ARev")
+                         "\\|")))
+
 (sml/setup)
