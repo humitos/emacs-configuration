53e1a9170dd97eb70780aa8b8a159536dc2c283e
Manuel Kaufmann <humitos@gmail.com>
2018-03-20 20:10:36 -0500

    More evil confs

--- startup.d/evil.el
+++ startup.d/evil.el
@@ -17,12 +17,20 @@
 ;; call `helm-find-files' easily from NORMAL mode
 (define-key evil-normal-state-map (kbd "g f") 'helm-find-files)
 
+;; call `helm-mini' easily from NORMAL mode
+(define-key evil-normal-state-map (kbd "g b") 'helm-mini)
+
+;; call `helm-etags-select' easily from NORMAL mode
+(define-key evil-normal-state-map (kbd "g t") 'helm-etags-select)
+
 ;; scroll with SPC in NORMAL mode
 (define-key evil-normal-state-map (kbd "SPC") 'evil-scroll-down)
 (define-key evil-normal-state-map (kbd "S-SPC") 'evil-scroll-up)
 
 ;; Disable evil on Circe
 (evil-set-initial-state 'circe-mode 'emacs)
+;; Disable evil on imenu-list
+(evil-set-initial-state 'imenu-list-mode 'emacs)
 
 (setq evil-move-cursor-back nil)
 (setq evil-want-fine-undo t)
