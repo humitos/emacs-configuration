4ae908694b23a698a471f28e6a9579668b540668
Manuel Kaufmann <humitos@gmail.com>
2018-03-23 20:50:52 -0500

    Move smart mode line require after variables declaration

--- startup.d/smart-mode-line.el
+++ startup.d/smart-mode-line.el
@@ -1,5 +1,4 @@
 ;; https://github.com/Malabarba/smart-mode-line
-(require 'smart-mode-line)
 (setq sml/theme 'respectful)
 (setq sml/no-confirm-load-theme t)
 (setq rm-blacklist
@@ -24,4 +23,5 @@
                            "ARev")
                          "\\|")))
 
+(require 'smart-mode-line)
 (sml/setup)
