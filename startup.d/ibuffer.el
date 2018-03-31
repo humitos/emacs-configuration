cb30a9ae393c15033b438a22b97736241567e427
Manuel Kaufmann <humitos@gmail.com>
2018-03-31 12:41:21 -0500

    Fix whitespaces

--- startup.d/ibuffer.el
+++ startup.d/ibuffer.el
@@ -15,48 +15,48 @@
 ;; Modify the default ibuffer-formats
 (setq ibuffer-formats
       '((mark modified read-only " "
-	      (name 18 18 :left :elide)
-	      " "
-	      (size-h 9 -1 :right)
-	      " "
-	      (mode 16 16 :left :elide)
-	      " "
-	      filename-and-process)))
+              (name 18 18 :left :elide)
+              " "
+              (size-h 9 -1 :right)
+              " "
+              (mode 16 16 :left :elide)
+              " "
+              filename-and-process)))
 
 ;; Group ibuffers
 ;; http://emacs-fu.blogspot.com/2010/02/dealing-with-many-buffers-ibuffer.html
 (setq ibuffer-saved-filter-groups
       (quote (("default"
-	       ("Org" ;; all org-related buffers
-		(mode . org-mode))
-	       ("Mail"
-		(or  ;; mail-related buffers
-		 (mode . message-mode)
-		 (mode . mail-mode)
-		 ;; etc.; all your mail related modes
-		 ))
-	       ("Python"
-		(mode . python-mode))
-	       ("HTML"
-		(mode . html-mode))
-	       ("Javascript"
-		(mode . js-mode))
-	       ("CSS"
-		(mode . css-mode))
-	       ("Emacs"
-		(mode . emacs-lisp-mode))
-	       ;; ("Programming" ;; prog stuff not already in MyProjectX
-	       ;; 	(or
-	       ;; 	 (mode . c-mode)
-	       ;; 	 (mode . perl-mode)
-	       ;; 	 (mode . python-mode)
-	       ;; 	 (mode . emacs-lisp-mode)
-	       ;; 	 ;; etc
-	       ;; 	 ))
-	       ("ERC"   (mode . erc-mode))
-	       ))))
+               ("Org" ;; all org-related buffers
+                (mode . org-mode))
+               ("Mail"
+                (or  ;; mail-related buffers
+                 (mode . message-mode)
+                 (mode . mail-mode)
+                 ;; etc.; all your mail related modes
+                 ))
+               ("Python"
+                (mode . python-mode))
+               ("HTML"
+                (mode . html-mode))
+               ("Javascript"
+                (mode . js-mode))
+               ("CSS"
+                (mode . css-mode))
+               ("Emacs"
+                (mode . emacs-lisp-mode))
+               ;; ("Programming" ;; prog stuff not already in MyProjectX
+               ;;       (or
+               ;;        (mode . c-mode)
+               ;;        (mode . perl-mode)
+               ;;        (mode . python-mode)
+               ;;        (mode . emacs-lisp-mode)
+               ;;        ;; etc
+               ;;        ))
+               ("ERC"   (mode . erc-mode))
+               ))))
 
 ;; Apply deafult ibuffer grouping
 (add-hook 'ibuffer-mode-hook
-	  (lambda ()
-	    (ibuffer-switch-to-saved-filter-groups "default")))
+          (lambda ()
+            (ibuffer-switch-to-saved-filter-groups "default")))
