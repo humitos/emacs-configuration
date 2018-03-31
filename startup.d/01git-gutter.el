f5eba6f8040213ba643b7cf4757005441befdc9c
Manuel Kaufmann <humitos@gmail.com>
2018-03-31 12:31:13 -0500

    Indent

--- startup.d/01git-gutter.el
+++ startup.d/01git-gutter.el
@@ -1,20 +1,20 @@
 ;; https://github.com/syohex/emacs-git-gutter-fringe
 (use-package git-gutter-fringe
-             :config
-             (custom-set-variables
-              '(git-gutter:unchanged-sign "  ") ;; two spaces
-              '(git-gutter:modified-sign "MM")
-              '(git-gutter:added-sign "++")     ;; multiple character is OK
-              '(git-gutter:deleted-sign "--")
-              '(git-gutter:lighter " GG")  ;; change name of minor-mode in mode
-                                           ;; line. first character should be a " "
-                                           ;; (space)
-              '(git-gutter:verbosity 0)  ;; Don't need log/message
-              '(git-gutter:update-interval 5)
-              )
-             (setq-default left-fringe-width 15)
-             (setq-default right-fringe-width 15)
+  :config
+  (custom-set-variables
+   '(git-gutter:unchanged-sign "  ") ;; two spaces
+   '(git-gutter:modified-sign "MM")
+   '(git-gutter:added-sign "++")     ;; multiple character is OK
+   '(git-gutter:deleted-sign "--")
+   '(git-gutter:lighter " GG")  ;; change name of minor-mode in mode
+   ;; line. first character should be a " "
+   ;; (space)
+   '(git-gutter:verbosity 0)  ;; Don't need log/message
+   '(git-gutter:update-interval 5)
+   )
+  (setq-default left-fringe-width 15)
+  (setq-default right-fringe-width 15)
 
-             ;; show git status (add, remove, etc lines) next to the row numbers
-             (global-git-gutter-mode +1)
-             )
+  ;; show git status (add, remove, etc lines) next to the row numbers
+  (global-git-gutter-mode +1)
+  )
