39eff8eb8982881308efef078dae32add61cde92
Manuel Kaufmann <humitos@gmail.com>
2018-03-31 12:35:50 -0500

    Material theme migrated

--- startup.d/99material-theme.el
+++ startup.d/99material-theme.el
@@ -1,14 +1,15 @@
 ;; https://github.com/cpaulik/emacs-material-theme
+(use-package material-theme
+  :after git-gutter-fringe
+  :config
+  (load-theme 'material t) ;; load material theme
 
-(add-to-list 'custom-theme-load-path (concat emacs-user-directory "vendor/emacs-material-theme"))
-(load-theme 'material t) ;; load material theme
+  ;; I need to load these settings here, because they are overiden by
+  ;; the "(load-theme 'material t)" function
 
-
-;; I need to load these settings here, because they are overiden by
-;; the "(load-theme 'material t)" function
-
-;; git-gutter background configuration for material theme
-(set-face-background 'git-gutter:unchanged "#263238")
-(set-face-background 'git-gutter:added "#263238")
-(set-face-background 'git-gutter:deleted "#263238")
-(set-face-background 'git-gutter:modified "#263238")
+  ;; git-gutter background configuration for material theme
+  (set-face-background 'git-gutter:unchanged "#263238")
+  (set-face-background 'git-gutter:added "#263238")
+  (set-face-background 'git-gutter:deleted "#263238")
+  (set-face-background 'git-gutter:modified "#263238")
+  )
