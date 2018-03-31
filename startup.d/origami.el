39ab8c72c849cc568102d02deac815d2a435115a
Manuel Kaufmann <humitos@gmail.com>
2018-03-31 12:53:25 -0500

    Origami only for python code

--- startup.d/origami.el
+++ startup.d/origami.el
@@ -1,10 +1,9 @@
 ;; https://github.com/gregsexton/origami.el
 (use-package origami
+  :hook (python-mode . origami-mode)
   :bind
   (()
    :map origami-mode-map
    ("C-c f a" . origami-toggle-all-nodes)
    ;; http://stackoverflow.com/questions/916797/emacs-global-set-key-to-c-tab
-   ("<C-tab>" . origami-recursively-toggle-node))
-  :config
-  (global-origami-mode 1))
+   ("<C-tab>" . origami-recursively-toggle-node)))
