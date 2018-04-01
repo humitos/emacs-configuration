a440517f846ba4d23cf8e45658491b43e2e2e05e
Manuel Kaufmann <humitos@gmail.com>
2018-03-31 19:36:57 -0500

    Avoid highlighting the .

--- startup.d/user-packages.el
+++ startup.d/user-packages.el
@@ -189,7 +189,7 @@
   (setq highlight-symbol-ignore-list
         '("False" "True" "None" "self" "def" "import" "from" "if" "else" "for"
           "while" "class" "print" "and" "not" "is" "param" "rtype" "async"
-          "with")))
+          "with" ".")))
 
 
 ;; https://github.com/purcell/less-css-mode.git
